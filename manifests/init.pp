3183334d565b90a9ebbd6e7db6052857cf8c4cb6
Christopher Petersen <chris@petersen.io>
2013-07-20 15:20:26 -0700

    Different install command

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,9 +10,8 @@ class macruby {
     cwd     => "/tmp",
     creates => "/tmp/MacRuby 0.12/MacRuby 0.12.pkg"
   }
-  package { 'MacRuby':
-    provider => 'package',
-    source   => '/tmp/MacRuby 0.12/MacRuby 0.12.pkg'
+  exec { 'Install MacRuby package':
+    command => 'sudo /usr/sbin/installer -verbose -pkg /tmp/MacRuby 0.12/MacRuby 0.12.pkg -target /'
   }
   exec { 'Add rbenv macruby plugin':
     command => "git clone git://github.com/brettg/rbenv-macruby.git",
