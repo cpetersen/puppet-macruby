416f5bdd313c54e495dee2c19f368facc636630b
Christopher Petersen <chris@petersen.io>
2013-07-20 12:28:16 -0700

    Another way of referencing the HOME directory

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,13 @@
 # This is a placeholder class.
 class macruby {
   exec { 'Create rbenv plugin directory':
-    command => "mkdir -p $HOME/.rbenv/plugins",
-    creates => "$HOME/.rbenv/plugins"
+    command => "mkdir -p .rbenv/plugins",
+    creates => ".rbenv/plugins"
   }
   exec { 'Add rbenv macruby plugin':
     command => "git clone git://github.com/brettg/rbenv-macruby.git",
-    cwd     => "$HOME/.rbenv/plugins",
-    creates => "$HOME/.rbenv/plugins/rbenv-macruby/libexec/rbenv-macruby"
+    cwd     => ".rbenv/plugins",
+    creates => ".rbenv/plugins/rbenv-macruby/libexec/rbenv-macruby"
   }
   exec { 'Init rbenv macruby':
     command => "rbenv macruby init"
