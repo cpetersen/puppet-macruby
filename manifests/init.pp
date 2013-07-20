438fd19e3471a0fa4bff0e28118698362f051d94
Christopher Petersen <chris@petersen.io>
2013-07-20 12:47:08 -0700

    Reverting

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,9 @@
 # This is a placeholder class.
 class macruby {
-  exec { 'Create rbenv plugin directory':
-    command => "mkdir -p .rbenv/plugins",
-    creates => ".rbenv/plugins"
-  }
   exec { 'Add rbenv macruby plugin':
     command => "git clone git://github.com/brettg/rbenv-macruby.git",
-    cwd     => ".rbenv/plugins",
-    creates => ".rbenv/plugins/rbenv-macruby/libexec/rbenv-macruby"
+    cwd     => "/opt/boxen/rbenv/plugins",
+    creates => "/opt/boxen/rbenv/plugins/rbenv-macruby/libexec/rbenv-macruby"
   }
   exec { 'Init rbenv macruby':
     command => "rbenv macruby init"
