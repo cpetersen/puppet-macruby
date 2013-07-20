3e126e61aba1ad51f53ce767b79382dc4bf49722
Christopher Petersen <chris@petersen.io>
2013-07-20 12:59:06 -0700

    Install MacRuby before rbenv plugin

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,10 @@
 # This is a placeholder class.
 class macruby {
+  package { 'MacRuby':
+    provider => 'compressed_pkg',
+    source   => 'https://github.com/downloads/MacRuby/MacRuby/MacRuby%200.12.zip'
+  }
+
   exec { 'Add rbenv macruby plugin':
     command => "git clone git://github.com/brettg/rbenv-macruby.git",
     cwd     => "/opt/boxen/rbenv/plugins",
