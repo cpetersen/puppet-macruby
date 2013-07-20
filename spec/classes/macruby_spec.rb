b4ca3c1d865eec215b030ad392d2d5a501de10f2
Christopher Petersen <chris@petersen.io>
2013-07-20 11:33:22 -0700

    Push trying macruby in the user's HOME direcory

--- spec/classes/macruby_spec.rb
+++ spec/classes/macruby_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'macruby' do
   it do
+    should contain_exec('Create rbenv plugin directory')
     should contain_exec('Add rbenv macruby plugin')
     should contain_exec('Init rbenv macruby')
   end
