86181f2deff642fd465593fafa59e2d124563857
Christopher Petersen <chris@petersen.io>
2013-07-20 12:16:01 -0700

    Small tweak in path references

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,13 @@
 # This is a placeholder class.
 class macruby {
   exec { 'Create rbenv plugin directory':
-    command => "mkdir -p ~/.rbenv/plugins",
+    command => "mkdir -p $HOME/.rbenv/plugins",
     creates => "~/.rbenv/plugins"
   }
   exec { 'Add rbenv macruby plugin':
     command => "git clone git://github.com/brettg/rbenv-macruby.git",
-    cwd     => "~/.rbenv/plugins",
-    creates => "~/.rbenv/plugins/rbenv-macruby/libexec/rbenv-macruby"
+    cwd     => "$HOME/.rbenv/plugins",
+    creates => "$HOME/.rbenv/plugins/rbenv-macruby/libexec/rbenv-macruby"
   }
   exec { 'Init rbenv macruby':
     command => "rbenv macruby init"
