941527dda0de8f01f659f0eb217a101872eb2d14
Christopher Petersen <chris@petersen.io>
2013-07-20 15:14:46 -0700

    One more try

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,19 @@
 # This is a placeholder class.
 class macruby {
-  package { 'MacRuby':
-    provider => 'compressed_pkg',
-    source   => 'https://github.com/downloads/MacRuby/MacRuby/MacRuby%200.12.zip'
+  exec { 'Download MacRuby':
+    command => "curl -L https://github.com/downloads/MacRuby/MacRuby/MacRuby%200.12.zip > macruby.zip",
+    cwd     => "/tmp",
+    creates => "/tmp/macruby.zip"
   }
-
+  exec { 'Unzip MacRuby':
+    command => "/usr/bin/unzip -o macruby.zip",
+    cwd     => "/tmp",
+    creates => "/tmp/MacRuby 0.12/MacRuby 0.12.pkg"
+  }
+  package { 'MacRuby':
+    provider => 'package',
+    source   => '/tmp/MacRuby 0.12/MacRuby 0.12.pkg'
+  }
   exec { 'Add rbenv macruby plugin':
     command => "git clone git://github.com/brettg/rbenv-macruby.git",
     cwd     => "/opt/boxen/rbenv/plugins",
