73bf438de1703a7b90f3ec21234b8c8154b081b2
Christopher Petersen <chris@petersen.io>
2013-07-20 16:49:19 -0700

    Added some requirements

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,24 +1,28 @@
 # This is a placeholder class.
 class macruby {
-  exec { 'Download MacRuby':
+  exec { 'download':
     command => "curl -L https://github.com/downloads/MacRuby/MacRuby/MacRuby%200.12.zip > macruby.zip",
     cwd     => "/tmp",
     creates => "/tmp/macruby.zip"
   }
-  exec { 'Unzip MacRuby':
+  exec { 'extract':
     command => "/usr/bin/unzip -o /tmp/macruby.zip",
     cwd     => "/tmp",
-    creates => "/tmp/MacRuby 0.12/MacRuby 0.12.pkg"
+    creates => "/tmp/MacRuby 0.12/MacRuby 0.12.pkg",
+    require => Exec["download"]
   }
-  exec { 'Install MacRuby package':
-    command => 'sudo /usr/sbin/installer -verbose -pkg /tmp/MacRuby 0.12/MacRuby 0.12.pkg -target /'
+  exec { 'install':
+    command => 'sudo /usr/sbin/installer -verbose -pkg /tmp/MacRuby 0.12/MacRuby 0.12.pkg -target /',
+    require => Exec["extract"]
   }
-  exec { 'Add rbenv macruby plugin':
+  exec { 'plugin':
     command => "git clone git://github.com/brettg/rbenv-macruby.git",
     cwd     => "/opt/boxen/rbenv/plugins",
-    creates => "/opt/boxen/rbenv/plugins/rbenv-macruby/libexec/rbenv-macruby"
+    creates => "/opt/boxen/rbenv/plugins/rbenv-macruby/libexec/rbenv-macruby",
+    require => Exec["install"]
   }
-  exec { 'Init rbenv macruby':
-    command => "rbenv macruby init"
+  exec { 'init':
+    command => "rbenv macruby init",
+    require => Exec["plugin"]
   }
 }
