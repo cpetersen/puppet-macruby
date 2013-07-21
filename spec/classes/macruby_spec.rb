237935bb8850aba696c89c1e58b78541f8cea62d
Christopher Petersen <chris@petersen.io>
2013-07-20 17:05:22 -0700

    Fixed the specs

--- spec/classes/macruby_spec.rb
+++ spec/classes/macruby_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe 'macruby' do
   it do
-    should contain_exec('Create rbenv plugin directory')
-    should contain_exec('Add rbenv macruby plugin')
-    should contain_exec('Init rbenv macruby')
+    should contain_exec('download')
+    should contain_exec('extract')
+    should contain_exec('install')
+    should contain_exec('plugin')
+    should contain_exec('init')
   end
 end
