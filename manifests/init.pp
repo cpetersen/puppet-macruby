43aaeed34226eb6d8a33109401c6bb6d93c5dc51
Christopher Petersen <chris@petersen.io>
2013-10-01 16:25:11 -0700

    Fixed some spec warnings

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,31 +1,31 @@
 # This is a placeholder class.
 class macruby {
-  exec { 'download':
-    command => "curl -L https://github.com/downloads/MacRuby/MacRuby/MacRuby%200.12.zip -o macruby.zip",
-    cwd     => "/tmp",
-    creates => "/tmp/macruby.zip"
+  exec { 'macruby-download':
+    command => 'curl -L https://github.com/downloads/MacRuby/MacRuby/MacRuby%200.12.zip -o macruby.zip',
+    cwd     => '/tmp',
+    creates => '/tmp/macruby.zip'
   }
-  exec { 'extract':
-    command => "/usr/bin/unzip -o /tmp/macruby.zip",
-    cwd     => "/tmp",
-    creates => "/tmp/MacRuby 0.12/MacRuby 0.12.pkg",
-    require => Exec["download"]
+  exec { 'macruby-extract':
+    command => '/usr/bin/unzip -o /tmp/macruby.zip',
+    cwd     => '/tmp',
+    creates => '/tmp/MacRuby 0.12/MacRuby 0.12.pkg',
+    require => Exec['macruby-download']
   }
-  exec { 'install':
+  exec { 'macruby-install':
     command => 'sudo /usr/sbin/installer -verbose -pkg /tmp/MacRuby\ 0.12/MacRuby\ 0.12.pkg -target /',
-    user => 'root',
+    user    => 'root',
     creates => '/usr/local/bin/macruby',
-    require => Exec["extract"]
+    require => Exec['macruby-extract']
   }
-  exec { 'plugin':
-    command => "git clone git://github.com/brettg/rbenv-macruby.git",
-    cwd     => "/opt/boxen/rbenv/plugins",
-    creates => "/opt/boxen/rbenv/plugins/rbenv-macruby/libexec/rbenv-macruby",
-    require => Exec["install"]
+  exec { 'macruby-plugin':
+    command => 'git clone git://github.com/brettg/rbenv-macruby.git',
+    cwd     => '/opt/boxen/rbenv/plugins',
+    creates => '/opt/boxen/rbenv/plugins/rbenv-macruby/libexec/rbenv-macruby',
+    require => Exec['macruby-install']
   }
-  exec { 'init':
-    command => "rbenv macruby init",
+  exec { 'macruby-init':
+    command => 'rbenv macruby init',
     creates => '/opt/boxen/rbenv/versions/macruby-0.12/lib',
-    require => Exec["plugin"]
+    require => Exec['macruby-plugin']
   }
 }
--- spec/classes/macruby_spec.rb
+++ spec/classes/macruby_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe 'macruby' do
   it do
-    should contain_exec('download')
-    should contain_exec('extract')
-    should contain_exec('install')
-    should contain_exec('plugin')
-    should contain_exec('init')
+    should contain_exec('macruby-download')
+    should contain_exec('macruby-extract')
+    should contain_exec('macruby-install')
+    should contain_exec('macruby-plugin')
+    should contain_exec('macruby-init')
   end
 end
